ff0becdb04efb44845e39268be933ca408e80f86
altyshkin <29455komandor@mail.ru>
2022-01-18 01:47:28 +0400

    component ClassicalMusic внесен с помощью аннотации @Autowired

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -5,19 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ClassicalMusic implements Music{
     // создание приватного конструктора , чтобы нельзся было создавать его объекты через new
-    private ClassicalMusic(){
-
-    }
-    //создание Fabric-method
-    public static ClassicalMusic getClassicalMusic(){
-        return  new ClassicalMusic();
-    }
-    public void doMyInit(){
-        System.out.println("Doing my initialization");
-    }
-    public void doMyDestroy(){
-        System.out.println("Doing my destroy");
-    }
     @Override
     public String getSong() {
         return "rapGod";
--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,40 +1,18 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicPlayer {
-    private Music music;
-    private String name;
-    private int volume ;
+    private ClassicalMusic classicalMusic;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    //IOK
-    public MusicPlayer(Music music){
-        this.music = music;
-
-    }
-    public MusicPlayer(){
-
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
+    @Autowired
+    public void setMusic(ClassicalMusic classicalMusic) {
+        this.classicalMusic = classicalMusic;
     }
 
     public void playMusic(){
-        System.out.println("Playing:"+ music.getSong());
+        System.out.println("Playing:"+ classicalMusic.getSong());
     }
 }
