12a07bc8a06e1b8a0f3dc3339df40a7b39919ff3
altyshkin <29455komandor@mail.ru>
2022-01-18 01:56:54 +0400

    Внедрение спрингом зависимости через приватное поле засчет рефлексии

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -5,16 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+    @Autowired
     private Music music;
 
 
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-    @Autowired
-   public void  setwMusic(){
-        this.music = music;
-   }
 
     public void playMusic(){
         System.out.println("Playing:"+ music.getSong());
